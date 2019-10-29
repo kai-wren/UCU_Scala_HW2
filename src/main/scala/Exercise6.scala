52e7945278bdc709f87539b98b4c5abbc9101efd
Kai Wren <miguel.manukyanez@gmail.com>
2019-10-29 19:30:23 +0200

    Ex 6 add right methods

--- src/main/scala/Exercise6.scala
+++ src/main/scala/Exercise6.scala
@@ -1,58 +1,76 @@
 object Exercise6 extends App {
 
-  trait ConsInterface[+A] {
+  trait lListInterface[+A] {
     def value: A
-    def next: ConsInterface[A]
+    def ref: lListInterface[A]
 
     def length(): Int
     def last(): A
-    def add[B >: A](addValue: B): ConsInterface[B] = Cons(addValue, this)
-    def remove(): ConsInterface[A] = next
-    def reverse(): ConsInterface[A] = {
-      val r = NilCons.add(value)
-    }
-  }
-
-  case class Cons[+A] (val value: A, val next: ConsInterface[A]) extends ConsInterface[A] {
-    override def toString = s"head: $value, next: $next"
-
-    override def length(): Int = 1 + next.length
-
-    override def last(): A = next match {
-      case NilCons => value
-      case _ => next.last()
+    def addLeft[B >: A](addValue: B): lListInterface[B] = lList(addValue, this)
+    def remove(): lListInterface[A] = ref
+    def reverse[B >: A](list: lListInterface[B] = NilList): lListInterface[B] = list match {
+        case NilList => ref.reverse(list.addLeft(value))
+        case _ if (ref == NilList) => list.addLeft(value)
+        case _ => ref.reverse(list.addLeft(value))
+      }
+    def addRight[B >: A](addValue: B, list: lListInterface[B] = this): lListInterface[B] = list match {
+//      case NilList => list
+      case _ if (ref == NilList) => list
+//      case _ => ref.addRight(addValue, list.addLeft(value))
+      case _ => list.addLeft(value).addRight(addValue, list.ref)
     }
 
-//    override def add[B >: A](addValue: B): ConsInterface[B] = super.add(addValue)
   }
 
-  object NilCons extends ConsInterface[Nothing] {
-    def value = throw new NoSuchElementException("head of empty list")
-    def next = throw new UnsupportedOperationException("tail of empty list")
+  case class lList[+A] (val value: A, val ref: lListInterface[A]) extends lListInterface[A] {
+
+    override def toString = s"head: $value, next: $ref"
+
+    override def length(): Int = 1 + ref.length
+
+    override def last(): A = ref match {
+      case NilList => value
+      case _ => ref.last()
+    }
+
+  }
+
+  object NilList extends lListInterface[Nothing] {
+
+    def value = throw new NoSuchElementException("No value")
+
+    def ref = throw new UnsupportedOperationException("No reference")
 
     override def length(): Int = 0
 
-    override def last(): Nothing = throw new NoSuchElementException("head of empty list")
+    override def last(): Nothing = throw new NoSuchElementException("No value")
 
-    //    override def add[B >: Nothing](addValue: B): ConsInterface[B] = super.add(addValue)
-    override def remove(): ConsInterface[Nothing] = throw new UnsupportedOperationException("tail of empty list")
+    override def remove(): lListInterface[Nothing] = throw new UnsupportedOperationException("No reference")
+
+    override def reverse[B >: Nothing](list: lListInterface[B]): lListInterface[B] = throw new UnsupportedOperationException("No reference")
   }
 
 
-    val c1 = NilCons.add(1)
-    val c2 = c1.add(2)
-    val c3 = c2.add(3)
-    val c4 = c3.add(4)
+    val c1 = NilList.addLeft(1)
+    val c2 = c1.addLeft(2)
+    val c3 = c2.addLeft(3)
+    val c4 = c3.addLeft(4)
     val n3 = c4.remove()
+    val r4 = c4.reverse()
+    val c5 = c4.addRight(5)
     println(c1)
     println(c2)
     println(c3)
     println(c4)
-    print(n3)
+    println(n3)
+    println(r4)
+    println(c5)
 
-    println(c4.length())
 
-    println(c4.last())
+    assert(c4.length() == 4)
+
+    assert(c4.last() == 1)
+    assert(r4.last() == 4)
 
 
 }
